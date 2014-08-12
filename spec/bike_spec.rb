26692dfe811e04c785cab7d0be46919f85e00db7
globalavocado <globalavocado@gmail.com>
2014-08-12 14:09:38 +0100

    tutorial up to second successful rspec test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -9,5 +9,11 @@ it "should not be broken after we create it" do
 		# expect is a method, the_bike is an instance
 		# be_broken is also a method
 	expect(bike).not_to be_broken
-	end
+end
+
+it "should be able to break" do
+		bike = Bike.new
+		bike.break!
+	expect(bike).to be_broken
+end
 end
