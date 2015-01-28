f4b82787fd4706332457298ddba322aead89559d
globalavocado <globalavocado@users.noreply.github.com>
2015-01-28 15:05:48 +0000

    refactored bike test

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,26 +1,22 @@
-# link to bike class
 require './lib/bike'
 
-# describing functionality of a specific class, that of bike
 describe Bike do
 
 	let(:bike){Bike.new}
 
-it "should not be broken after we create it" do
-		# expect is a method, the_bike is an instance
-		# be_broken is also a method
-	expect(bike).not_to be_broken
-end
+	it "should not be broken after we create it" do
+		expect(bike).not_to be_broken
+	end
 
-it "should be able to break" do
-		bike.break!
-	expect(bike).to be_broken
-end
+	it "should be able to break" do
+			bike.break!
+		expect(bike).to be_broken
+	end
 
-it "should be able to get fixed" do
-		bike.break!
-		bike.fix!
-	expect(bike).not_to be_broken
-end
+	it "should be able to get fixed" do
+			bike.break!
+			bike.fix!
+		expect(bike).not_to be_broken
+	end
 
 end
